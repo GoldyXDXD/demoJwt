3e967ed75e23dc0f238dd81cdc7f448e90df352f
GoldyXDXD <teamnscafe@yandex.ru>
2021-07-27 20:36:00 +0300

    Создание проекта

---
+++ src/test/java/org/goldyxdxd/demoJwt/DemoJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.goldyxdxd.demoJwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
